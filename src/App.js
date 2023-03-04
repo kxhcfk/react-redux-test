412cacf689a2b95ec4f71226b6ab99f7a95b24eb
Maksim Sirotkin <reveersed@mail.ru>
2023-03-04 16:15:57 +0300

    reducers, store, combine reducers, redux thunk

--- src/App.js
+++ src/App.js
@@ -1,9 +1,58 @@
-import React from 'react';
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import "./App.css";
+import { addCustomerAction, removeCustomerAction } from "./store/reducers/customerReducer";
+import { fetchCustomers } from "./asyncAction/customers";
 
 const App = () => {
+	const dispatch = useDispatch();
+	const cash = useSelector(state => state.cash.cash);
+	const customers = useSelector(state => state.customers.customers);
+	
+	function addCash(cash) {
+		dispatch({ type: "ADD_CASH", payload: cash });
+	}
+	
+	function getCash(cash) {
+		dispatch({ type: "GET_CASH", payload: cash });
+	}
+	
+	function addCustomer(name) {
+		const customer = {
+			name,
+			id: Date.now(),
+		};
+		dispatch(addCustomerAction(customer));
+	}
+	
+	function removeCustomer(customer) {
+		dispatch(removeCustomerAction(customer.id));
+	}
+	
 	return (
-		<div>
-		
+		<div className={"App"}>
+			<h1>{cash}</h1>
+			<div className="flex">
+				<button onClick={() => addCash(Number(prompt()))}>+</button>
+				<button onClick={() => getCash(Number(prompt()))}>-</button>
+				<button onClick={() => addCustomer(prompt())}>Добавить клиента</button>
+				<button onClick={() => dispatch(fetchCustomers())}>Получить клиентов</button>
+			</div>
+			
+			{!customers.length
+				? <h2>ПУСТО</h2>
+				: (
+					<div>
+						{customers.map(customer => (
+							<div
+								key={customer.id}
+								className="customer"
+								onClick={() => removeCustomer(customer)}
+							>{customer.name}</div>
+						))}
+					</div>
+				)
+			}
 		</div>
 	);
 };
--- src/index.js
+++ src/index.js
@@ -1,10 +1,62 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { Provider } from "react-redux";
+import { store } from "./store";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+/* import { createStore } from 'redux';
+
+/!* action = {
+	type: string,
+	action: object,
+} *!/
+
+const defaultState = {
+	cash: 0,
+};
+
+const cashReducer = (state = defaultState, action) => {
+	switch (action.type) {
+		case 'ADD_CASH':
+			return {
+				...state,
+				cash: state.cash + action.payload,
+			};
+		case 'GET_CASH':
+			return {
+				...state,
+				cash: state.cash - action.payload,
+			};
+		default:
+			return state;
+	}
+};
+
+const customerReducer = (state = defaultState, action) => {
+	switch (action.type) {
+		case 'ADD_CUSTOMER':
+			return {
+				...state,
+				cash: state.cash + action.payload,
+			};
+		case 'GET_CUSTOMERS':
+			return {
+				...state,
+				cash: state.cash - action.payload,
+			};
+		default:
+			return state;
+	}
+};
+
+const store = createStore(reducer); */
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
-		<App/>
+		<Provider store={store}>
+			<App/>
+		</Provider>
 	</React.StrictMode>,
 );
